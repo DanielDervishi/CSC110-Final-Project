6faad30232a833a057a835acb4312a5fedfc9b74
David De Martin <59452411+DrDabbidy@users.noreply.github.com>
2021-12-13 17:11:46 -0500

    Add main block to heatmap_generation.py

--- heatmap_generation.py
+++ heatmap_generation.py
@@ -104,3 +104,18 @@ def month_year_to_str(month: int, year: int) -> str:
     """
     months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
     return f'{months[month - 1]} {year}'
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    
+    import python_ta
+    python_ta.check_all(config={
+        'extra-imports': ['plotly', 'pandas', 'json', 'crime_data', 'dash'],
+        'max-line-length': 100,
+        'disable': ['R1705', 'C0200']
+    })
+    
+    import python_ta.contracts
+    python_ta.contracts.DEBUG_CONTRACTS = False
+    python_ta.contracts.check_all_contracts()
